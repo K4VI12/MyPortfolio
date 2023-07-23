f2c3078fc9d4aecfc17c99e744e3f07715f04940
kavindu hansaka <kavinduhansaka755@gmail.com>
2023-07-24 01:31:51 +0530

    modify

---
+++ script.js
@@ -0,0 +1,18 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(Links => {
+                Links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
