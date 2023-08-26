633ff58193d0c652341ee03af64380695f79534d
kavindu hansaka <kavinduhansaka755@gmail.com>
2023-08-26 18:20:09 +0530

    modify

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// let sections = document.querySelectorAll('section');
-// let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150;
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-//         if(top >= offset && top < offset + height) {
-//             navLinks.forEach(Links => {
-//                 Links.classList.remove('active');
-//                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-//             });
-//         };
-//     });
-// };
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(Links => {
+                Links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
 
 document.addEventListener("DOMContentLoaded", function() {
     // Get all the navigation links
