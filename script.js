c3f48fde8e0d1596a269b48748b033a852c664b2
kavindu hansaka <kavinduhansaka755@gmail.com>
2023-08-30 19:55:48 +0530

    modify

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+
+};
+
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -8,29 +18,40 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height) {
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(Links => {
                 Links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
     });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
 
+
+
 ScrollReveal({
-    reset: true,
+    // reset: true,
     distance: '80px',
     duration: 2000,
     delay: 250
 });
 
-ScrollReveal().reveal('.home-content, .heading',{ origin:'top' })
-ScrollReveal().reveal('.home-img, .services-container, .projects-box, .contact',{ origin:'bottom' })
-ScrollReveal().reveal('.home-content h1, .about',{ origin:'left' })
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .projects-box, .contact', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Fullstack Developer','Frontend Developer','Web Developer'],
+    strings: ['Fullstack Developer', 'Frontend Developer', 'Web Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
